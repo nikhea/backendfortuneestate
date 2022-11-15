e3dc436665d5d3faee464e9b14647399cef3fd11
nikhea <imonikheaugbodaga@gmail.com>
2022-11-15 20:40:00 +0100

    added express

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("")
+import express from "express"
+
+const app = express()
+
+
+
+let port = process.env.PORT;
+app.listen(port, () => {
+  console.log("Server is up and running on port number " + port);
+});
