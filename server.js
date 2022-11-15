20037f64802d2f668bf7e4f1f860a45f77c2a6e6
nikhea <imonikheaugbodaga@gmail.com>
2022-11-15 20:45:44 +0100

    started the app server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from "express"
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+import Express from "express";
 
-const app = express()
-
-
+const app = Express();
 
 let port = process.env.PORT;
 app.listen(port, () => {
