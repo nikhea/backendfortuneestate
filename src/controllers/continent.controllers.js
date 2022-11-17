f41ffdafbc595780cf37e4b7276887be09004ae3
nikhea <imonikheaugbodaga@gmail.com>
2022-11-17 03:10:16 +0100

    created endpoints to create AND get continents

--- src/controllers/continent.controllers.js
+++ src/controllers/continent.controllers.js
@@ -21,3 +21,57 @@ export const getContinents = async (req, res, next) => {
     return res.json(response);
   }
 };
+
+export const createContinents = async (req, res, next) => {
+  try {
+    let continents = new Continents({
+      name: req.body.name,
+      bgImage: req.body.bgImage,
+      image: req.body.image,
+    });
+    console.log(continents + " continents");
+    await continents.save();
+    if (continents) {
+      let response = {
+        success: "true",
+        statuscode: 200,
+        data: continents,
+        message: "success",
+      };
+      res.json(response);
+    }
+  } catch (error) {
+    console.error(error.message);
+    let response = {
+      statuscode: 400,
+      data: [],
+      error: [error],
+      message: "something failed",
+    };
+    return res.json(response);
+  }
+};
+export const getOneContinents = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    let continents = await Continents.findById(id);
+    console.log(id + " id");
+    console.log(continents + " continents");
+    let response = {
+      success: "true",
+      statuscode: 200,
+      data: continents,
+      message: "success",
+    };
+    res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    let response = {
+      statuscode: 400,
+      data: [],
+      error: [error],
+      message: "something failed",
+    };
+    return res.json(response);
+  }
+};
--- src/routes/continent.routes.js
+++ src/routes/continent.routes.js
@@ -1,12 +1,23 @@
 import express from "express";
-import { getContinents } from "../controllers/continent.controllers.js";
+import {
+  getContinents,
+  createContinents,
+  getOneContinents,
+} from "../controllers/continent.controllers.js";
 const router = express.Router();
 // @route     GET api/continent
-// @desc      Get logged in user
+// @desc      Get  all continents
 //@access     Public
 router.get("/", getContinents);
 
+// @route     POST api/continent
+// @desc      create a new continent
+//@access     Public
+router.post("/", createContinents);
 
+// @route     GET api/continent
+// @desc      GET one continent
+//@access     Public
+router.get("/:id", getOneContinents);
 
-
-export default router
+export default router;
