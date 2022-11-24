42ea39b8b7081c7ef958e6d452ac28ece7cdb166
nikhea <imonikheaugbodaga@gmail.com>
2022-11-24 17:53:38 +0100

    added users modal

---
+++ src/models/user.model.js
@@ -0,0 +1,40 @@
+import * as mongoose from "mongoose";
+import { roles } from "../utils/constants";
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: [roles.admin, roles.moderator, roles.client],
+    default: roles.client,
+  },
+});
+const users = mongoose.model("Users", UserSchema);
+
+export default users;
